152b67efb7eefdb188ff2390ee67145fba8fabed
Alekcandr Dmitriev <126895869+alpshoot@users.noreply.github.com>
2023-05-29 11:08:06 +0300

    Add files via upload

--- PP_2_1_3_SpringBeans-master/src/main/java/app/model/AnimalsCage.java
+++ PP_2_1_3_SpringBeans-master/src/main/java/app/model/AnimalsCage.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class AnimalsCage {
 
-    @Autowired
+    private Animal animal;
     private Timer timer;
+
+    @Autowired
+    public AnimalsCage(Timer timer, @Qualifier("dog") Animal animal) {
+        this.timer = timer;
+        this.animal = animal;
+    }
 
     public Timer getTimer() {
         return timer;
     }
-
-    @Autowired
-    @Qualifier("dog")
-    private Animal animal;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
